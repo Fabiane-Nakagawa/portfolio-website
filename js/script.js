35ded85ec1b73c20f63da97c7fba679811d52fa6
Creative initial version <>
2018-08-17 22:39:33 -0300

    adjust css

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-console.log(“Hi! Welcome to My Portfolio Site”)
+console.log('Hi! Welcome to My Portfolio Site')
+console.log("He said: \"Yes!\", that's for sure",'He said: "Yes!", that\'s for sure')
+
+for (var i = 1; i <= 100; i++) {
+  console.log(i);
+}

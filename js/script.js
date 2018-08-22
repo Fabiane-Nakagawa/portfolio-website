45e7f7be9ecd7ef64e214c2c352c85fb61464ca7
Creative initial version <>
2018-08-22 09:35:39 -0300

    include jquery and js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 console.log('Hi! Welcome to My Portfolio Site')
-console.log("He said: \"Yes!\", that's for sure",'He said: "Yes!", that\'s for sure')
 
-for (var i = 1; i <= 100; i++) {
-  console.log(i);
-}
+
+$(document).ready(function() {
+  $(".project-preview").on("click", function() {
+    $(this).siblings(".project-details").slideToggle();
+    });
+});
